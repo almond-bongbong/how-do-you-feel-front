21dda0c1a945378526d98dfc7f33b4f2100d1321
cmlee <bal.dongdongdong@gmail.com>
2022-08-17 21:38:33 +0900

    :white_check_mark: add test code

--- components/common/form/button/button.test.js
+++ components/common/form/button/button.test.js
@@ -12,9 +12,40 @@ describe('Button', () => {
 
   it('onClick 테스트', () => {
     const onClick = jest.fn();
-    const { getByText } = render(<Button onClick={onClick}>테스트</Button>);
-    fireEvent.click(getByText('테스트'));
+    render(<Button onClick={onClick}>테스트</Button>);
+    fireEvent.click(screen.getByText('테스트'));
 
     expect(onClick).toHaveBeenCalled();
+  });
+
+  it('disabled 테스트', () => {
+    const onClick = jest.fn();
+    render(
+      <Button disabled onClick={onClick}>
+        테스트
+      </Button>,
+    );
+    fireEvent.click(screen.getByText('테스트'));
+
+    expect(onClick).not.toHaveBeenCalled();
+  });
+
+  it('loading 테스트', () => {
+    const onClick = jest.fn();
+    render(
+      <Button loading onClick={onClick}>
+        테스트
+      </Button>,
+    );
+    fireEvent.click(screen.getByText('테스트'));
+
+    expect(onClick).not.toHaveBeenCalled();
+  });
+
+  it('auto focus 테스트', () => {
+    render(<Button autoFocus>테스트</Button>);
+    const button = screen.getByText('테스트', { selector: 'button' });
+
+    expect(button).toHaveFocus();
   });
 });
