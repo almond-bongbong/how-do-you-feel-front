5fea348723a4f8ff16a38b7d048313c8e9322a8b
cmlee <bal.dongdongdong@gmail.com>
2022-09-30 19:38:49 +0900

    :construction: update map header

--- components/map/map-navigator/map-navigator.tsx
+++ components/map/map-navigator/map-navigator.tsx
@@ -2,17 +2,31 @@ import React from 'react';
 import classNames from 'classnames/bind';
 import styles from './map-navigator.module.scss';
 import Link from 'next/link';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome } from '@fortawesome/pro-solid-svg-icons';
+import { faChevronLeft } from '@fortawesome/pro-light-svg-icons';
+import { useRouter } from 'next/router';
 
 const cx = classNames.bind(styles);
 
 function MapNavigator() {
+  const router = useRouter();
+
   return (
     <div className={cx('container')}>
-      <h1>
-        <Link href="/">
-          <a>홈으로</a>
-        </Link>
-      </h1>
+      <header className={cx('header')}>
+        <button type="button" className={cx('prev_button')} onClick={router.back}>
+          <FontAwesomeIcon icon={faChevronLeft} />
+          이전 페이지로 이동
+        </button>
+        <h1 className={cx('logo')}>
+          <Link href="/">
+            <a>
+              <FontAwesomeIcon icon={faHome} />
+            </a>
+          </Link>
+        </h1>
+      </header>
     </div>
   );
 }
